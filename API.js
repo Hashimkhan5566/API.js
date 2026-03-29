154bd0345e7b0fff5c1683f4e59dede4a97414eb
Hashim khan <hashimkhanlazak0301@gmail.com>
2026-03-29 23:45:07 +0500

    today we add and practice on previous topic of javascript

--- API.js
+++ API.js
@@ -1,15 +1,87 @@
 fetch("https://api.crossref.org/works")
-  .then((value2)=>{
-    return value2.json();
-  }).then((value1)=>{
-    console.log(value1);
-  }).catch((err)=>{
-    console.log(err);
+.then((res)=>{
+  return res.json();
+}).then((value1)=>{
+  console.log(value1);
+  
+}).then((value2)=>{
+  console.log(value2);
+  return value2;
+  
+}).catch((err)=>{
+  console.log(err);
+  
+});
+
+//promise
+let promise = new Promise ((resolve,reject)=>{
+   setTimeout(()=>{
+    resolve (5);
+   }, 2000);
+}).then((value)=>{
+  console.log(value);
+  return value;
+}).then((value2)=>{
+  console.log(value2);
+  
+})
+let promise2 =  new Promise((resolve, reject)=>{
+  setTimeout(() => {
+     reject(3);
+  },3000);
+
+})
+promise2.then((value4)=>{
+  console.log(value4);
+  
+}).catch((err)=>{
+  console.log("this is error show on top");
+  
+})
+promise2.then((value3)=>{
+  console.log(value3);
+  
+}).catch((err)=>{
+  console.log("this is error show on top");
+  
+});
+
+let promise3 = new Promise ((resolve)=>{
+  setTimeout(() => {
+    resolve (34);
+  },3000);
+})
+  let promise4 = new Promise ((resolve)=>{
+  setTimeout(() => {
+    resolve (35);
+  },2000);
+ 
+
+}) ;
+Promise.all([promise3, promise4])
+.then((value4)=>{
+  console.log(value4);
+  
+}).catch((err)=>{
+  console.log("this is error show on top and bottom");
+  
+})
+
+async function harry(){
+  let lahoreweather  =  new Promise((resolve, reject)=>{
+    setTimeout(() => {
+      resolve("27 deg");
+    }, 2000);
+  });
+  let punjabweather = new Promise((resolve, reject)=>{
+    setTimeout(() => {
+      resolve("29deg"); 
+    },3000);
   });
 
+  let laW =  await lahoreweather;
+  let pw = await punjabweather;
+  return [laW, pw];
+}
+harry();
 
-  let a = 12;
-  document.cookies="harry";
-  document.cookies = a;
- console.log(( document.cookies));
-                
