1f1e9a662c977afd042b4a392af300d504d419fa
Hashim khan <hashimkhanlazak0301@gmail.com>
2026-04-03 02:28:44 +0500

    today we practice on previous topic of css and html

--- API.js
+++ API.js
@@ -1,191 +1,192 @@
-// fetch("https://api.crossref.org/works")
-// .then((res)=>{
-//   return res.json();
-// }).then((value1)=>{
-//   console.log(value1);
+// // fetch("https://api.crossref.org/works")
+// // .then((res)=>{
+// //   return res.json();
+// // }).then((value1)=>{
+// //   console.log(value1);
   
-// }).then((value2)=>{
-//   console.log(value2);
-//   return value2;
+// // }).then((value2)=>{
+// //   console.log(value2);
+// //   return value2;
   
-// }).catch((err)=>{
-//   console.log(err);
+// // }).catch((err)=>{
+// //   console.log(err);
   
-// });
+// // });
 
-// //promise
-// let promise = new Promise ((resolve,reject)=>{
-//    setTimeout(()=>{
-//     resolve (5);
-//    }, 2000);
-// }).then((value)=>{
-//   console.log(value);
-//   return value;
-// }).then((value2)=>{
-//   console.log(value2);
+// // //promise
+// // let promise = new Promise ((resolve,reject)=>{
+// //    setTimeout(()=>{
+// //     resolve (5);
+// //    }, 2000);
+// // }).then((value)=>{
+// //   console.log(value);
+// //   return value;
+// // }).then((value2)=>{
+// //   console.log(value2);
   
-// })
-// let promise2 =  new Promise((resolve, reject)=>{
-//   setTimeout(() => {
-//      reject(3);
-//   },3000);
+// // })
+// // let promise2 =  new Promise((resolve, reject)=>{
+// //   setTimeout(() => {
+// //      reject(3);
+// //   },3000);
 
-// })
-// promise2.then((value4)=>{
-//   console.log(value4);
+// // })
+// // promise2.then((value4)=>{
+// //   console.log(value4);
   
-// }).catch((err)=>{
-//   console.log("this is error show on top");
+// // }).catch((err)=>{
+// //   console.log("this is error show on top");
   
-// })
-// promise2.then((value3)=>{
-//   console.log(value3);
+// // })
+// // promise2.then((value3)=>{
+// //   console.log(value3);
   
-// }).catch((err)=>{
-//   console.log("this is error show on top");
+// // }).catch((err)=>{
+// //   console.log("this is error show on top");
   
-// });
+// // });
 
-// let promise3 = new Promise ((resolve)=>{
-//   setTimeout(() => {
-//     resolve (34);
-//   },3000);
-// })
-//   let promise4 = new Promise ((resolve)=>{
-//   setTimeout(() => {
-//     resolve (35);
-//   },2000);
+// // let promise3 = new Promise ((resolve)=>{
+// //   setTimeout(() => {
+// //     resolve (34);
+// //   },3000);
+// // })
+// //   let promise4 = new Promise ((resolve)=>{
+// //   setTimeout(() => {
+// //     resolve (35);
+// //   },2000);
  
 
-// }) ;
-// Promise.all([promise3, promise4])
-// .then((value4)=>{
-//   console.log(value4);
+// // }) ;
+// // Promise.all([promise3, promise4])
+// // .then((value4)=>{
+// //   console.log(value4);
   
-// }).catch((err)=>{
-//   console.log("this is error show on top and bottom");
+// // }).catch((err)=>{
+// //   console.log("this is error show on top and bottom");
   
-// })
+// // })
 
-// async function harry(){
-//   let lahoreweather  =  new Promise((resolve, reject)=>{
-//     setTimeout(() => {
-//       resolve("27 deg");
-//     }, 2000);
-//   });
-//   let punjabweather = new Promise((resolve, reject)=>{
-//     setTimeout(() => {
-//       resolve("29deg"); 
-//     },3000);
-//   });
+// // async function harry(){
+// //   let lahoreweather  =  new Promise((resolve, reject)=>{
+// //     setTimeout(() => {
+// //       resolve("27 deg");
+// //     }, 2000);
+// //   });
+// //   let punjabweather = new Promise((resolve, reject)=>{
+// //     setTimeout(() => {
+// //       resolve("29deg"); 
+// //     },3000);
+// //   });
 
-//   let laW =  await lahoreweather;
-//   let pw = await punjabweather;
-//   return [laW, pw];
-// }
-// harry();
-console.log(document.body.firstChild);
-console.log(document.body.firstChild.previousSibling);
+// //   let laW =  await lahoreweather;
+// //   let pw = await punjabweather;
+// //   return [laW, pw];
+// // }
+// // harry();
+// console.log(document.body.firstChild);
+// console.log(document.body.firstChild.previousSibling);
 
-// async function harry(){
-//   let promise  =  new Promise((resolve, reject)=>{
-//     setTimeout(() => {
-//        resolve(34);
-//     },3000);
+// // async function harry(){
+// //   let promise  =  new Promise((resolve, reject)=>{
+// //     setTimeout(() => {
+// //        resolve(34);
+// //     },3000);
 
-//   })
+// //   })
 
-//   let promise2 = new Promise((resolve, reject)=>{
-//     setTimeout(() => {
-//       resolve(40);
-//     }, 4000);
-//   })
+// //   let promise2 = new Promise((resolve, reject)=>{
+// //     setTimeout(() => {
+// //       resolve(40);
+// //     }, 4000);
+// //   })
 
-//   let p1 = await promise;
-//   let p2 = await promise2;
+// //   let p1 = await promise;
+// //   let p2 = await promise2;
 
-//   return [p1,p2];
-// }
-//  const cherry = ()=>{
-// console.log("welcome to promise");
-// }
-// let main=()=>{
-
-
-// let a =  harry();
-// let b = cherry();
-// console.log(a);
-// console.log(b);
-
-// }
-// main();
+// //   return [p1,p2];
+// // }
+// //  const cherry = ()=>{
+// // console.log("welcome to promise");
+// // }
+// // let main=()=>{
 
 
-// setTimeout(() => {
-//   console.log("deep");
+// // let a =  harry();
+// // let b = cherry();
+// // console.log(a);
+// // console.log(b);
+
+// // }
+// // main();
+
+
+// // setTimeout(() => {
+// //   console.log("deep");
   
-// },3000);
+// // },3000);
 
-// setTimeout(() => {
-//   console.log("concept");
+// // setTimeout(() => {
+// //   console.log("concept");
   
-// }, 4000);
-// try{
-// setTimeout(() => {
-//   try{
-//   console.log(deep);
-//   }
-//   catch(err){
-//     console.log("error this same");
-//     console.log(err.name);
-//     console.log(err.message);
-//     console.log(err.stack);
+// // }, 4000);
+// // try{
+// // setTimeout(() => {
+// //   try{
+// //   console.log(deep);
+// //   }
+// //   catch(err){
+// //     console.log("error this same");
+// //     console.log(err.name);
+// //     console.log(err.message);
+// //     console.log(err.stack);
     
     
     
-//   }
-// }, 6000);
+// //   }
+// // }, 6000);
+// // }
+// // catch(err){
+// //   console.log("balle balle");
+  
+// // }
+// // setTimeout(() => {
+// //   console.log("run");
+  
+// // }, 5000);
+// // let num = Number(prompt("enter your number"));
+// // if(num>10){
+// //   throw new ReferenceError("your number is greater");
+  
+// // }
+
+
+// //currency converter
+// let amount  = Number(prompt("enter your amount"));
+// let currency  = prompt("pkr", "us", "darham");
+// let rate ;
+// if(currency==="us"){
+//   rate = 280;
+  
+
 // }
-// catch(err){
-//   console.log("balle balle");
+// else if(currency==="pkr"){
+//   rate = 409;
   
 // }
-// setTimeout(() => {
-//   console.log("run");
-  
-// }, 5000);
-// let num = Number(prompt("enter your number"));
-// if(num>10){
-//   throw new ReferenceError("your number is greater");
+// else if(currency==="darham"){
+//   rate = 78;
   
 // }
+// let formula = amount*rate;
+// // let inp = document.getElementsByClassName(input)[0];
+// // let btn = document.getElementsByClassName(button)[0];
 
-
-//currency converter
-let amount  = Number(prompt("enter your amount"));
-let currency  = prompt("pkr", "us", "darham");
-let rate ;
-if(currency==="us"){
-  rate = 280;
-  
-
-}
-else if(currency==="pkr"){
-  rate = 409;
-  
-}
-else if(currency==="darham"){
-  rate = 78;
-  
-}
-let formula = amount*rate;
-// let inp = document.getElementsByClassName(input)[0];
-// let btn = document.getElementsByClassName(button)[0];
-
-// btn.addEventListener("click" , function(){
+// // btn.addEventListener("click" , function(){
     
-// });
-alert(formula);
+// // });
+// alert(formula);
 
-//example of dom
-document.body.style.backgroundColor = "red";
+// //example of dom
+// document.body.style.backgroundColor = "red";
+
